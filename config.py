6b94346f28c2a73f1286c6254107a4f80eb84169
Shunmugam V <shunmugamv@Shunmugams-MacBook-Pro-2.local>
2022-05-18 15:16:12 +0530

    modified the create script

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
-HOST='http://localhost:3000'
-USER_NAME='admin'
-PASSWORD='admin@123'
+# HOST='http://localhost:3000'
+# USER_NAME='admin'
+# PASSWORD='admin@123'
+# TOKEN='79fd165ae59ea0bea56c7d22c0aef7a6be303428'
+
+
+# https://try.gitea.io/
+HOST='https://try.gitea.io/api/v1'
+USER_NAME='ishnmu'
+PASSWORD=''
+TOKEN='79fd165ae59ea0bea56c7d22c0aef7a6be303428'
---
+++ create-repos-from-template.py
@@ -0,0 +1,26 @@
+import requests
+import config
+
+TEMLPATE_NAME='sample'
+header = {
+    'Authorization': f'token {config.TOKEN}',
+    "Content-Type" : "application/json",
+    "accept": "application/json" 
+}
+
+for i in range(1,3):
+    URL = f"{config.HOST}/repos/{config.USER_NAME}/{TEMLPATE_NAME}/generate"
+    payload = {
+        'name': f'repo-{i}',
+        'owner': f'{config.USER_NAME}',
+        'default_branch': 'master',
+        'description': f'repo created from api - repo{i}',
+        'git_content': True,
+        'git_hooks': True,
+        'labels': True,
+        'private': True,
+        'topics': True,
+        'webhooks': True
+    }
+    r = requests.post(url = URL, json=payload, headers=header)
+    print(f"Status code for repo-{i} creation", r.status_code, r.json(), URL, payload)
