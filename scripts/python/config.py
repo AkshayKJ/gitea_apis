4841469641dea45ac77410c3cf5f52234280a958
Shunmugam V <shunmugamv@Shunmugams-MacBook-Pro-2.local>
2022-05-19 15:56:48 +0530

    modify config

--- scripts/python/config.py
+++ scripts/python/config.py
@@ -1,29 +1,17 @@
 HOST='http://localhost:3000'
-USER_NAME='admin123'
-PASSWORD='admin@123'
-TOKEN='cc6a3a8733fd412673a58f15cf75f605ced86cef'
+USER_NAME='gittea'
+PASSWORD='gittea'
+TOKEN='ec54b83ae911ecbc61c6c3e06aa3515c2f845884'
 GOCD_TOKEN= 'c46943e4a64c8864adfd35616cf79e21aa2ebabb'
-
-
-# https://try.gitea.io/
-
-# HOST='https://try.gitea.io'
-# USER_NAME='ishnmu'
-# PASSWORD=''
-# TOKEN='79fd165ae59ea0bea56c7d22c0aef7a6be303428'
-
+GOCD_HOST= 'http://192.168.56.11:8153/'
 
 HEADER = {
     'Authorization': f'token {TOKEN}',
     "Content-Type" : "application/json",
     "accept": "application/json" 
 }
-
-
 GOCD_HEADER = {
     'Authorization':  f'bearer {GOCD_TOKEN}',
     "Content-Type" : "application/json",
     "accept": "application/vnd.go.cd.v4+json" 
 }
-
-GOCD_HOST= 'http://192.168.56.11:8153/'
